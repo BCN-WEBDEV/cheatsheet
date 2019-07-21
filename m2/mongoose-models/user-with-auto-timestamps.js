5a9192dc84c0d9ea3602693093cfdd0450a498da
tawebbcn <ta-web-bcn@ironhack.com>
2019-07-21 18:09:15 +0200

    Update user-with-auto-timestamps.js

--- m2/mongoose-models/user-with-auto-timestamps.js
+++ m2/mongoose-models/user-with-auto-timestamps.js
@@ -6,7 +6,8 @@ const Schema   = mongoose.Schema;
 const userSchema = new Schema({
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
