9e1b92b6e4df03c26ac2da42aa27c4aa5cb07318
tawebbcn <ta-web-bcn@ironhack.com>
2019-07-25 08:11:32 +0200

    Update cloudinary.js

--- m2/cloudinary.js
+++ m2/cloudinary.js
@@ -1,6 +1,6 @@
-//npm install cloudinary multer-storage-cloudinary multer
+// npm install cloudinary multer-storage-cloudinary multer
 
-//in config folder new file cloudinary.js
+// In config folder new file cloudinary.js
 
 const cloudinary = require('cloudinary');
 const cloudinaryStorage = require('multer-storage-cloudinary');
@@ -26,21 +26,21 @@ const parser = multer({ storage: storage });
 module.exports = parser
 
 
-// in the hbs view
+
+// In the hbs view
 <form action="/meme" method="POST" enctype="multipart/form-data">
   <input type="file" name="photo" />
   <button type="submit">Add Image</button>
 </form>
 
 
-// in the router where we want to upload the image
+
+// In the router where we want to upload the image
 const parser = require('../config/cloudinary');
 
 router.post('/', parser.single('photo'), (req, res, next) =>{
  ...
- 
  const image_url = req.file.secure_url
- 
  ...
 }
 
